27a459a18cb2d680e68a52d62a6eccab5af88aa9
Terry Burns-Dyson <terrybd@gmail.com>
2018-11-25 13:06:49 -0500

    Make recall a dual purpose command but not too different

--- twitchbot/commands/RecallCommand.cs
+++ twitchbot/commands/RecallCommand.cs
@@ -8,12 +8,41 @@ using twitchbot.models;
 
 namespace twitchbot.commands
 {
+    [TwitchCommand("recall", Ignore = true)]
     public class RecallCommand : ITwitchCommand
     {
-        private readonly string _commandResult;
+        private readonly string _command;
+
+        public RecallCommand()
+        {
+            _command = String.Empty;
+        }
 
         public RecallCommand(string command)
         {
+            _command = command;
+        }
+
+        public string Execute(params string[] args)
+        {
+            string commandDefinition = string.Empty;
+
+            if (!string.IsNullOrEmpty(_command))
+            {
+                commandDefinition = LoadCommand(_command);
+            }
+            else if (args.Length > 0)
+            {
+                commandDefinition = LoadCommand(args[0]);
+            }
+
+            return commandDefinition;
+        }
+
+        private string LoadCommand(string command)
+        {
+            string result = string.Empty;
+
             using (StreamReader reader = new StreamReader("definitions.json"))
             {
                 string content = reader.ReadToEnd();
@@ -23,16 +52,12 @@ namespace twitchbot.commands
                     IEnumerable<CommandDefinition> definedCommmands =
                         JsonConvert.DeserializeObject<IEnumerable<CommandDefinition>>(content);
 
-                    _commandResult = definedCommmands.Single(cmd =>
+                    result = definedCommmands.Single(cmd =>
                         cmd.Command.Equals(command, StringComparison.CurrentCultureIgnoreCase)).Content;
                 }
             }
-        }
 
-
-        public string Execute(params string[] args)
-        {
-            return _commandResult;
+            return result;
         }
     }
 }
