d1f1d8a87dd78fa97d9bda0cdec77750f1eb65cb
Terry Burns-Dyson <terrybd@gmail.com>
2019-05-31 20:04:54 -0400

    Organized parsers, created basic SO command

---
+++ twitchstreambot/Commands/ShoutOutCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using twitchstreambot.infrastructure;
+
+namespace twitchstreambot.commands
+{
+    [TwitchCommand("so")]
+    public class ShoutOutCommand : ITwitchCommand
+    {
+        public bool CanExecute(IDictionary<string, string> headers)
+        {
+            if (headers.ContainsKey("badges"))
+            {
+                var badges = headers["badges"].ToLower();
+
+                return badges.Contains("broadcaster") ||
+                       badges.Contains("moderator");
+            }
+
+            return false;
+        }
+
+        public string Execute(params string[] args)
+        {
+            if (!args.Any())
+            {
+                return "so <username>";
+            }
+
+            return $"Checkout {args.First()} at http://twitch.tv/{args.First()}";
+        }
+    }
+}
