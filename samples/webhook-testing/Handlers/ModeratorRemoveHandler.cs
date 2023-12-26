0f0bb506eeccf515b3bef4a52d70b226007777d1
Terry Burns-Dyson <terrybd@gmail.com>
2023-12-26 17:00:53 -0500

    Remove event and handler

---
+++ samples/webhook-testing/Handlers/ModeratorRemoveHandler.cs
@@ -0,0 +1,14 @@
+﻿using twitchstreambot.webhooks.Events.Channel.Moderator;
+using twitchstreambot.webhooks.Infrastructure;
+
+namespace webhook_testing.Handlers;
+
+public class ModeratorRemoveHandler : TwitchWebhookEventHandler<Remove>
+{
+    public override Task Handle(PublishedEvent<Remove> @event, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Moderator removed: {@event.Event.UserName}");
+
+        return Task.CompletedTask;
+    }
+}
---
+++ twitchstreambot.webhooks/Events/Channel/Moderator/Remove.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+using twitchstreambot.webhooks.Events.Contracts;
+using twitchstreambot.webhooks.Infrastructure.Attributes;
+
+namespace twitchstreambot.webhooks.Events.Channel.Moderator;
+
+[WebhookEvent("channel.moderator.remove")]
+public class Remove : WebhookBaseEvent, IContainBroadcasterInformation, IContainUserInformation
+{
+    [JsonPropertyName("broadcaster_user_id")]
+    public string BroadcasterUserId { get; set; }
+
+    [JsonPropertyName("broadcaster_user_name")]
+    public string BroadcasterUserName { get; set; }
+
+    [JsonPropertyName("broadcaster_user_login")]
+    public string BroadcasterUserLogin { get; set; }
+
+    [JsonPropertyName("user_id")] public string? UserId { get; set; }
+    [JsonPropertyName("user_name")] public string? UserName { get; set; }
+    [JsonPropertyName("user_login")] public string? UserLogin { get; set; }
+}
