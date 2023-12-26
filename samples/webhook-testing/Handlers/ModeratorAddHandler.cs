c1856b204fd8459c172a93d2cf5c9a35f77966d6
Terry Burns-Dyson <terrybd@gmail.com>
2023-12-26 16:59:18 -0500

    Moderator Add Event with test handler

---
+++ samples/webhook-testing/Handlers/ModeratorAddHandler.cs
@@ -0,0 +1,14 @@
+﻿using twitchstreambot.webhooks.Events.Channel.Moderator;
+using twitchstreambot.webhooks.Infrastructure;
+
+namespace webhook_testing.Handlers;
+
+public class ModeratorAddHandler : TwitchWebhookEventHandler<Add>
+{
+    public override Task Handle(PublishedEvent<Add> @event, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Moderator added: {@event.Event.UserName}");
+
+        return Task.CompletedTask;
+    }
+}
--- twitchstreambot.webhooks/Events/Channel/Moderator/Add.cs
+++ twitchstreambot.webhooks/Events/Channel/Moderator/Add.cs
@@ -1,6 +1,22 @@
-﻿namespace twitchstreambot.webhooks.Events.Channel.Moderator;
+﻿using System.Text.Json.Serialization;
+using twitchstreambot.webhooks.Events.Contracts;
+using twitchstreambot.webhooks.Infrastructure.Attributes;
 
-public class Add
+namespace twitchstreambot.webhooks.Events.Channel.Moderator;
+
+[WebhookEvent("channel.moderator.add")]
+public class Add : WebhookBaseEvent, IContainBroadcasterInformation, IContainUserInformation
 {
-    
+    [JsonPropertyName("broadcaster_user_id")]
+    public string BroadcasterUserId { get; set; } = null!;
+
+    [JsonPropertyName("broadcaster_user_name")]
+    public string BroadcasterUserName { get; set; } = null!;
+
+    [JsonPropertyName("broadcaster_user_login")]
+    public string BroadcasterUserLogin { get; set; } = null!;
+
+    [JsonPropertyName("user_id")] public string? UserId { get; set; }
+    [JsonPropertyName("user_name")] public string? UserName { get; set; }
+    [JsonPropertyName("user_login")] public string? UserLogin { get; set; }
 }
