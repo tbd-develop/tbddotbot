e4d0b2ddc46bfab8374828d419d956c910d3ea5b
Terry Burns-Dyson <terrybd@gmail.com>
2023-12-27 15:59:50 -0500

    Charity Donation message / demo handler

---
+++ samples/webhook-testing/Handlers/CharityCampaign/DonateHandler.cs
@@ -0,0 +1,14 @@
+﻿using twitchstreambot.webhooks.Events.Channel.CharityCampaign;
+using twitchstreambot.webhooks.Infrastructure;
+
+namespace webhook_testing.Handlers.CharityCampaign;
+
+public class DonateHandler : TwitchWebhookEventHandler<Donate>
+{
+    public override Task Handle(PublishedEvent<Donate> @event, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Charity campaign donation: {@event.Message.Amount.Value} {@event.Message.Amount.Currency}");
+
+        return Task.CompletedTask;
+    }
+}
---
+++ twitchstreambot.webhooks/Events/Channel/CharityCampaign/Donate.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+using twitchstreambot.webhooks.Events.Contracts;
+using twitchstreambot.webhooks.Events.Values;
+using twitchstreambot.webhooks.Infrastructure.Attributes;
+
+namespace twitchstreambot.webhooks.Events.Channel.CharityCampaign;
+
+[WebhookEvent("channel.charity_campaign.donate")]
+public class Donate : WebhookBaseEvent, IContainBroadcasterInformation, IContainUserInformation
+{
+    public string Id { get; set; } = null!;
+
+    [JsonPropertyName("campaign_id")] public string CampaignId { get; set; } = null!;
+
+    [JsonPropertyName("charity_name")] public string CharityName { get; set; } = null!;
+
+    [JsonPropertyName("charity_description")]
+    public string CharityDescription { get; set; } = null!;
+
+    [JsonPropertyName("charity_logo")] public string CharityLogo { get; set; } = null!;
+    [JsonPropertyName("charity_website")] public string CharityWebsite { get; set; } = null!;
+
+    public MonetaryAmount Amount { get; set; } = null!;
+
+    [JsonPropertyName("broadcaster_user_id")]
+    public string BroadcasterUserId { get; set; } = null!;
+
+    [JsonPropertyName("broadcaster_user_name")]
+    public string BroadcasterUserName { get; set; } = null!;
+
+    [JsonPropertyName("broadcaster_user_login")]
+    public string BroadcasterUserLogin { get; set; } = null!;
+
+    [JsonPropertyName("user_id")] public string? UserId { get; set; }
+    [JsonPropertyName("user_name")] public string? UserName { get; set; }
+    [JsonPropertyName("user_login")] public string? UserLogin { get; set; }
+}
